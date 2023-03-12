1c88588dcb7097d367a3ab604224128b1cf8da17
benwade91 <70988204+benn925@users.noreply.github.com>
2023-03-12 10:00:53 -0700

    added content to icon

--- index.js
+++ index.js
@@ -9,11 +9,22 @@ function initMap() {
     mapId: "a4aeaf34cd1e581a",
   });
 
-  new google.maps.Marker({
+  const marker = new google.maps.Marker({
     position: { lat: 37.7749, lng: -122.4194 },
     map,
     title: "Hello World!",
     icon: icon,
+  });
+
+  const infowindow = new google.maps.InfoWindow({
+    content: "contentString",
+    ariaLabel: "Uluru",
+  });
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+    });
   });
 }
 
