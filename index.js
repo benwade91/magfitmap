330dbe6947d7dab94d929277834e68d869db4b16
benwade91 <70988204+benn925@users.noreply.github.com>
2023-03-12 12:23:29 -0700

    more data

--- index.js
+++ index.js
@@ -5,8 +5,37 @@ fetch("./data/gyms.json")
   .then((response) => response.json())
   .then((json) => {
     data = json.body;
-    window.initMap = initMap;
+    data.map(file => {
+      const marker = new google.maps.Marker({
+        position: {
+          lat: file.Latitude,
+          lng: file.Longitude,
+        },
+        map,
+        title: file.Name,
+        icon: image,
+      });
+
+      const infowindow = new google.maps.InfoWindow({
+        content: file.Name,
+        ariaLabel: "Uluru",
+      });
+      marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+        });
+      });
+    })
   });
+
+const image = {
+  url: "https://benwade.dev/magfitmap/markers/magfitIcon.png",
+  size: new google.maps.Size(71, 71),
+  origin: new google.maps.Point(0, 0),
+  anchor: new google.maps.Point(17, 34),
+  scaledSize: new google.maps.Size(50, 50)
+};
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -18,36 +47,5 @@ function initMap() {
     mapId: "a4aeaf34cd1e581a",
   });
 
-  const image = {
-    url: "https://benwade.dev/magfitmap/markers/magfitIcon.png",
-    size: new google.maps.Size(71, 71),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(17, 34),
-    scaledSize: new google.maps.Size(50, 50)
-  };
-
-  data.map(file => {
-
-    const marker = new google.maps.Marker({
-      position: {
-        lat: file.Latitude,
-        lng: file.Longitude,
-      },
-      map,
-      title: file.Name,
-      icon: image,
-    });
-
-    const infowindow = new google.maps.InfoWindow({
-      content: file.Name,
-      ariaLabel: "Uluru",
-    });
-    marker.addListener("click", () => {
-      infowindow.open({
-        anchor: marker,
-        map,
-      });
-    });
-  })
-
 }
+window.initMap = initMap;
